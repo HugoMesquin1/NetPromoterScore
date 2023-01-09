2699d916a1898b47c65bf8ade28fab79097d6d46
Hugo Mesquini <hugomesq1n1@gmail.com>
2023-01-09 17:14:11 -0300

    ADD: NPC Calcule

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
 import 'reflect-metadata'
 import { router } from './routes'
 import createConnection from "../src/database"
-import express from 'express'
+import express, { NextFunction } from 'express'
+import "express-async-errors" 
+import { Request,Response } from 'express'
 
 createConnection()
 
 import './database'
+import { AppError } from './errors/AppError'
 
 const app = express()
 app.use(express.json())
+
+app.use((err: Error, request:Request, response:Response, _next : NextFunction)=> {
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal servel error ${err.message}`
+    })
+})
 
 app.use(router)
 
